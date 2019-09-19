882e58a59326f71c07285eadfa93448aacb09cc3
jolaya182 <jolaya182@gmail.com>
2019-09-18 20:49:00 -0400

    completed the application. added number history to the form

--- README.md
+++ README.md
@@ -6,3 +6,5 @@ download using git clone git@github.com:jolaya182/backStage.git
 use you terminal or comand line of choice
 
 go to the src folder and run the command npm start and your main browser should appear with the application running
+
+or open the index.html located in the `dist` folder
--- app/src/components/HomeForm.js
+++ app/src/components/HomeForm.js
@@ -1,132 +1,137 @@
 import React from 'react';
 
-export default class HomeForm extends React.Component{
-    constructor(props){
+export default class HomeForm extends React.Component {
+    constructor(props) {
         super(props);
         this.state = {
-            value:"",
-            recentNum:"",
-            numberOfOccur:"",
-            recentDate:"",
-            lastDate:"",
-            cache:{}
+            value: "",
+            recentNum: "",
+            numberOfOccur: "",
+            recentDate: "",
+            lastDate: "",
+            cache: {},
+            memoized: false
         }
     }
-    sumAdditon = (num) =>{
-        return Math.pow((num*(num+1))/2, 2);
-    // const result = Math.pow( (num*(num+1))/2 , 2);
-    // console.log(result);
-        // return result;
-    }
-    
-    squareSumAddition = (num)=>{
-    return (num*(num+1)*((2*num)+1))/6;
-    // const result = (num*(num+1)*((2*num)+1))/6;
-    // console.log(result);
-        // return result;
-    }
-    
-    squareSumDiff = (num)=>{
-        return Math.abs(this.sumAdditon(num)-this.squareSumAddition(num));
+    sumAdditon = (num) => {
+        return Math.pow((num * (num + 1)) / 2, 2);
     }
 
-    onInputChange = (e)=>{
-        e.preventDefault();
-        console.log(e.target.value);
-        const recentNum = e.target.value;
-        if( !this.isNumber(recentNum)) {alert("not a valid number, please type in a number"); return;}
-        this.setState((state, props)=>({recentNum: recentNum}))// update the recent variables
-      }
-     
-    isNumber = (num)=>{
-        return isNaN(num) ? false: true;
+    squareSumAddition = (num) => {
+        return (num * (num + 1) * ((2 * num) + 1)) / 6;
+
     }
-    
-    submitRequest = (e)=>{
+
+    squareSumDiff = (num) => {
+        return Math.abs(this.sumAdditon(num) - this.squareSumAddition(num));
+    }
+
+    onInputChange = (e) => {
+        e.preventDefault();
+        const recentNum = e.target.value;
+        if (!this.isNumber(recentNum)) { alert("not a valid number, please type in a number"); return; }
+        this.setState((state, props) => ({ recentNum: recentNum }))// update the recent variables
+    }
+
+    isNumber = (num) => {
+        return isNaN(num) ? false : true;
+    }
+
+    submitRequest = (e) => {
         e.preventDefault();
         const recentOccurance = Number(e.target[0].value);
-        const squareSumDiff = this.squareSumDiff;
-        const contextualThis = this;
-        const checkCacheForAnswer = this.checkCacheForAnswer;
-        const state  = this.state;
-        const setState = this.setState;
-        //this needs to change
-        const value = this.squareSumDiff(recentOccurance)
-        console.log("value",value);
-        this.setState((state, props)=>({value: value}))// update the recent variables
-
         const currDate = new Date();
         const currTime = currDate.toLocaleString("en-US");
 
-        // promise
-        const reqProm = new Promise((resolve, reject)=>{
-          //async function, sever request
-          setTimeout(function(){
-            const calculation = squareSumDiff(recentOccurance);
-            const today = new Date();
-            const reob = {recentDate:today.toLocaleString("en-US") ,recentNum:recentOccurance ,  value: calculation, lastDate: currTime};
-            console.log(reob);
-            resolve(reob, contextualThis, state, setState);
-          })
-        }, 350);
+        if (!this.state.memoized) {
+            // promise
+            const reqProm = new Promise((resolve, reject) => {
+                //async function, sever request
+                setTimeout(() => {
+                    console.log("async Call");
+                    const calculation = this.squareSumDiff(recentOccurance);
+                    const today = new Date();
+                    const reob = { recentDate: today.toLocaleString("en-US"), recentNum: recentOccurance, value: calculation, lastDate: currTime };
+                    resolve(reob);
+                })
+            }, 350);
 
-        reqProm.then((result, contextualThis)=>{
-            this.updateNumberInfo(result, contextualThis, checkCacheForAnswer, state, setState);
-        })
+            reqProm.then((result) => {
+                this.updateNumberInfo(result);
+            })
 
-      }
+            return;
+        }
+        console.log("no async call");
+        const today = new Date();
+        const reob = { recentDate: today.toLocaleString("en-US"), recentNum: recentOccurance, value: "", lastDate: currTime };
+        this.updateNumberInfo(reob)
+    }
 
-      updateNumberInfo(numObj, contextualThis, checkCacheForAnswer, state, setState){
-          console.log(numObj);
-          console.log(state)    
+    updateNumberInfo = (numObj) => {
         //update state recent variables
         //check if checkCacheForAnswer is answer or false
-        let num = 0;
-        if(checkCacheForAnswer()){
-            
-            const oldObj = state.cache[numObj.recentNum];
-          state.cache[numObj.recentNum] = {
-            value:numObj.value,
-            recentNum:numObj.recentNum,
-            numberOfOccur: oldObj + 1,
-            recentDate:numObj.recentDate,
-            lastDate:numObj.lastDate,
-          }// upate new data numObj.date, value, occurance, last date
-          setState((state)=>({cache: oldObj}))// update the cache
-        }else{
-            let cache = state.cache;
-          //create new data object and add it to the cache
-          // all its properties
-          const recentNum = {
-              value:numObj.value,
-            recentNum:numObj.recentNum,
-            numberOfOccur: oldObj + 1,
-            recentDate:numObj.recentDate,
-            lastDate:numObj.lastDate,}
-          cache[0][numObj.recentNum] = recentNum;
-          setState((state)=>({cache: cache}))// update the cache
-          ////this.setState();
+        if (this.checkCacheForAnswer(numObj.recentNum)) {
+
+            const oldObj = this.state.cache[numObj.recentNum];
+            // console.log("oldObj",oldObj)
+            let cache = this.state.cache;
+            const newObj = {
+                value: oldObj.value,
+                recentNum: numObj.recentNum,
+                numberOfOccur: oldObj.numberOfOccur + 1,
+                recentDate: numObj.recentDate,
+                lastDate: numObj.lastDate,
+            }// upate new data numObj.date, value, occurance, last date
+            cache[numObj.recentNum] = newObj;
+            this.setState((state, props) => ({ value: oldObj.value, cache: cache }))// update the cache
+        } else {
+            let cache = this.state.cache;
+            //create new data object and add it to the cache
+            // all its properties
+            const recentNum = {
+                value: numObj.value,
+                recentNum: numObj.recentNum,
+                numberOfOccur: 1,
+                recentDate: numObj.recentDate,
+                lastDate: numObj.lastDate,
+            }
+            cache[numObj.recentNum] = recentNum;
+            this.setState((state, props) => ({ value: numObj.value, cache: cache }))// update the cache
         }
-      
-      }
 
-        checkCacheForAnswer = (num)=>{
-        return this.state.cache[num] ?  this.state.cache[num] : false;
-      }
+    }
 
-    render(){
-        const { recentNum,numberOfOccur,lastDate, value } = this.state;
-        const {submitRequest, onInputChange} = this;
+    checkCacheForAnswer = (num) => {
+        return this.state.cache[num] ? this.state.cache[num] : false;
+    }
+
+    checkedBx = () => {
+        this.setState((state, props) => ({ memoized: !this.state.memoized }))
+    }
+
+    getRecentOccurencyHistory = () => {
+        if (this.state.cache[this.state.recentNum]) {
+            const { recentNum, numberOfOccur, lastDate } = this.state.cache[this.state.recentNum];
+            return { recentNum, numberOfOccur, lastDate };
+        }
+        return { recentNum: "", numberOfOccur: "", lastDate: "" };
+    }
+
+    render() {
+        const { memoized, value } = this.state;
+        const { submitRequest, onInputChange, checkedBx } = this;
+        const recentNumHistory = this.getRecentOccurencyHistory();
         return (<div>
             <form onSubmit={submitRequest}>
                 <input type="text" onChange={onInputChange}></input>
-                <label>{"answer:" }{value}</label>
-                <input type="submit" value="Submit"/>
-
+                <label>{"answer:"}{value}</label>
+                <input type="submit" value="Submit" />
+                <input type="checkbox" name="isMemoized" checked={memoized} onChange={checkedBx} /><label>memoized</label>
             </form>
-            <div>number:{recentNum}</div>
-            <div># occurences:{numberOfOccur}</div>
-            <div>last date:{lastDate}</div>
+            <div>number:{recentNumHistory.recentNum}</div>
+            <div># occurences:{recentNumHistory.numberOfOccur}</div>
+            <div>last date:{recentNumHistory.lastDate}</div>
         </div>);
     }
 }
